22629f81897ba8b88c377497ac84e5799f8fefe9
Wasl1 <merikawasselin@gmail.com>
2021-11-28 18:18:01 +0300

    mongoose

--- config/db.js
+++ config/db.js
@@ -4,9 +4,7 @@ mongoose
     .connect(process.env.MONGO_URI,
         {
             useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
+            useUnifiedTopology: true
         }
     )
     .then(() => console.log('Connected to MongoDB'))
